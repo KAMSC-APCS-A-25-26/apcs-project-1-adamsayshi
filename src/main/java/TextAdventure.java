91e2e1f7efcf9d434ff4a9710be22601d6a2d2f9
Adam <141070325+adamsayshi@users.noreply.github.com>
2025-10-28 14:04:15 -0400

    day1 class

--- src/main/java/TextAdventure.java
+++ src/main/java/TextAdventure.java
@@ -1,17 +1,66 @@
 import java.util.Scanner;
-
+import java.io.File;
 public class TextAdventure {
-    public static void main(String[] args) {
+    public class GlobalConstants{
+        public static String name;
+    }
+    public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
-        System.out.println("         ,-.-.     ,----.              _,.----.     _,.---._           ___      ,----.  \n" +
-                ",-..-.-./  \\==\\ ,-.--` , \\   _.-.    .' .' -   \\  ,-.' , -  `.  .-._ .'=.'\\  ,-.--` , \\ \n" +
-                "|, \\=/\\=|- |==||==|-  _.-` .-,.'|   /==/  ,  ,-' /==/_,  ,  - \\/==/ \\|==|  ||==|-  _.-` \n" +
-                "|- |/ |/ , /==/|==|   `.-.|==|, |   |==|-   |  .|==|   .=.     |==|,|  / - ||==|   `.-. \n" +
-                " \\, ,     _|==/==/_ ,    /|==|- |   |==|_   `-' \\==|_ : ;=:  - |==|  \\/  , /==/_ ,    / \n" +
-                " | -  -  , |==|==|    .-' |==|, |   |==|   _  , |==| , '='     |==|- ,   _ |==|    .-'  \n" +
-                "  \\  ,  - /==/|==|_  ,`-._|==|- `-._\\==\\.       /\\==\\ -    ,_ /|==| _ /\\   |==|_  ,`-._ \n" +
-                "  |-  /\\ /==/ /==/ ,     //==/ - , ,/`-.`.___.-'  '.='. -   .' /==/  / / , /==/ ,     / \n" +
-                "  `--`  `--`  `--`-----`` `--`-----'                `--`--''   `--`./  `--``--`-----``  ");
-        System.out.println("");
+        Scanner readmeFile = new Scanner(new File("src/main/readme.txt"));
+        //Initialize variables
+        boolean ContinuePlay = true;
+        int user;
+        //Introduction Screen
+        System.out.println(" ██▓     ▒█████   ██▒   █▓▓█████     ██▓  ██████  ██▓    ▄▄▄       ███▄    █ ▓█████▄ \n" +
+                "▓██▒    ▒██▒  ██▒▓██░   █▒▓█   ▀    ▓██▒▒██    ▒ ▓██▒   ▒████▄     ██ ▀█   █ ▒██▀ ██▌\n" +
+                "▒██░    ▒██░  ██▒ ▓██  █▒░▒███      ▒██▒░ ▓██▄   ▒██░   ▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌\n" +
+                "▒██░    ▒██   ██░  ▒██ █░░▒▓█  ▄    ░██░  ▒   ██▒▒██░   ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌\n" +
+                "░██████▒░ ████▓▒░   ▒▀█░  ░▒████▒   ░██░▒██████▒▒░██████▒▓█   ▓██▒▒██░   ▓██░░▒████▓ \n" +
+                "░ ▒░▓  ░░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░   ░▓  ▒ ▒▓▒ ▒ ░░ ▒░▓  ░▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒ \n" +
+                "░ ░ ▒  ░  ░ ▒ ▒░    ░ ░░   ░ ░  ░    ▒ ░░ ░▒  ░ ░░ ░ ▒  ░ ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒ \n" +
+                "  ░ ░   ░ ░ ░ ▒       ░░     ░       ▒ ░░  ░  ░    ░ ░    ░   ▒      ░   ░ ░  ░ ░  ░ \n" +
+                "    ░  ░    ░ ░        ░     ░  ░    ░        ░      ░  ░     ░  ░         ░    ░    \n" +
+                "                      ░                                                       ░      ");
+        System.out.println("-------------------------------------------------------------------------------------");
+        //Title Screen
+        while (ContinuePlay) {
+            System.out.println("1. Play\n2. Help\n3. Exit");
+            System.out.print("Write your option: ");
+            user = sc.nextInt();
+            //Switch case to Handle User Input
+            switch (user) {
+                case 1:
+                    //User is done playing
+                    Scene1();
+                    ContinuePlay = false;
+                    break;
+                case 2:
+                    while (readmeFile.hasNext()){
+                        System.out.println(readmeFile.nextLine());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    ContinuePlay = false;
+                    break;
+                default:
+                    System.out.println("I don't know that");
+                    break;
+            }
+        }
+        //Done Playing
+        System.out.println("Thanks for playing!");
+    }
+    static void Scene1(){
+        //Initialize variables for scene 1
+        Scanner sc = new Scanner(System.in);
+        //Dialog
+        System.out.print("What would you like your name to be?: ");
+        sc.nextLine();
+        GlobalConstants.name = sc.nextLine();
+        System.out.println("You wake up, with your mom yelling in your ear."); Thread.sleep(2000);
+        System.out.println("\"Why are you still in bed?? You are the most disappointing of all my sons\" - Mom"); Thread.sleep(1000);
+        System.out.println("\"I don't understand why you're mad, just let me find a job soon\" - " + GlobalConstants.name); Thread.sleep(1000);
+        System.out.println("\"I have waited long enough! Leave my house!\" - Mom"); Thread.sleep(1000);
     }
 }
---
+++ src/main/readme.txt
@@ -0,0 +1,34 @@
+You are a contestant on Love Island located in Australia, hoping to find the love of your life.
+However, your mom is incredibly disappointed that you have to go on a dating show to find a
+girlfriend. You ignore her and continue on with the show, with tears in your eyes. She kicked
+you out the house, so you better find a perfect girlfriend who will be your sugar mommy. This
+game will guide you through your life, and it will teach you the cost benefit analysis of
+various options and teach you several life lessons along the way.
+
+
+Title
+ - Play, Help, Exit
+Setting
+ - Located in Australia
+Scenes
+ 1. House - arguing with mom
+ 2. Living on the Street - Recruiter comes and asks you on love island - eventually say yes
+ 3. Riding on bus to Love Island place - guy on bus wants to bet all life savings with you
+    - Big decision - if you have money or not later on
+    - Guy on bus is a fake and part of the crew
+    - Find a paper on the floor that says "Help!" - maybe - secret
+ 4. You meet the contestants - 8 of them - cut half of them out
+    - Kind of a big decision
+ 5. Bed - something feels off about this place - eat drink and decide clothes?
+ 6. Next day - games with the contestants - cut half of them
+ 7. Last day - Choose between 2 - go on dates with each
+ 8. Go back out onto society
+    - bad ending - girl leaves so you're broke
+      - Live on the Streets and die
+    - good ending - Girl stays and you live happily ever after
+      - mom comes back, choose between dog and cat, choose if you want to run love island
+    - secret 1 - get all the girls
+    - secret 2 - uncover the mysteries of the Love Island
+
+Girls
+ 1.
